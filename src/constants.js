8ffdb633722bcaf86db55fb263e6d93e2597dda5
Adam Winick <awinick@criticalmass.com>
2019-04-25 11:58:03 -0400

    Original setup before adding the Nissan JSON file to go through URLs

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 const TEST_DIR = "screenshots-current";
-const ORIGINAL_DIR = "screenshots-golden";
+const ORIGINAL_DIR = "screenshots-original";
 const TEST_ROUTE = "http://www.adamwinick.com";
 const TEST_DIFFERENT_ROUTE = "https://news.ycombinator.com/";
 
 module.exports = {
-    TEST_DIR,
-    ORIGINAL_DIR,
-    TEST_ROUTE,
-    TEST_DIFFERENT_ROUTE,
-}
+  TEST_DIR,
+  ORIGINAL_DIR,
+  TEST_ROUTE,
+  TEST_DIFFERENT_ROUTE,
+};
