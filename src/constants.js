d4fbf6ded11cd0d17da109d0dd6035a00e939702
Adam Winick <awinick@criticalmass.com>
2019-04-25 16:41:17 -0400

    WIP line by line parser

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,12 @@ const TEST_DIR = "screenshots-current";
 const ORIGINAL_DIR = "screenshots-original";
 const TEST_ROUTE = "http://www.adamwinick.com";
 const TEST_DIFFERENT_ROUTE = "https://news.ycombinator.com/";
+const TXT_FILE = "nl-urls.mini.txt";
 
 module.exports = {
   TEST_DIR,
   ORIGINAL_DIR,
   TEST_ROUTE,
   TEST_DIFFERENT_ROUTE,
+  TXT_FILE,
 };
--- test/test.js
+++ test/test.js
@@ -1,24 +1,50 @@
 const puppeteer = require("puppeteer");
 const expect = require("chai").expect;
 const fs = require("fs");
+const readline = require("readline");
 const CONSTANTS = require("../src/constants");
 const PNG = require("pngjs").PNG;
 const pixelmatch = require("pixelmatch");
+const util = require("util");
 
-const { TEST_DIR, TEST_ROUTE, ORIGINAL_DIR, TEST_DIFFERENT_ROUTE } = CONSTANTS;
+const {
+  TEST_DIR,
+  TEST_ROUTE,
+  ORIGINAL_DIR,
+  TEST_DIFFERENT_ROUTE,
+  TXT_FILE,
+} = CONSTANTS;
 
 // mocha -g 'take'
 // mocha -g 'compare'
+
+// read a txt file line by line;
+const readFileLineByLine = async txtFile => {
+  const URLS = fsreadFileSync(txtFile, "utf-8")
+    .split("\n")
+    .filter(Boolean);
+
+  return Promise.resolve(URLS);
+};
+
 // - page is a reference to the Puppeteer page.
 // - route is the path you're loading, which I'm using to name the file.
 // - filePrefix is either "wide" or "narrow", since I'm automatically testing both.
-async function takeScreenshotToCompare(page, route, filePrefix) {
+async function takeScreenshotToCompare(page, route, index, filePrefix) {
   // If you didn't specify a file, use the name of the route.
-  let fileName = filePrefix + "/" + (route ? route : "index");
+  let fileName =
+    filePrefix +
+    "/" +
+    (index
+      ? index.charAt(0 === "/") && index.replace(/\//g, "-").substr(1)
+      : "index");
 
   // Start the browser, go to that page, and take a screenshot.
-  await page.goto(TEST_ROUTE);
-  await page.screenshot({ path: `${TEST_DIR}/${fileName}.png` });
+  await page.goto(route);
+  await page.screenshot({
+    path: `${TEST_DIR}/${fileName}.png`,
+    fullPage: true,
+  });
 
   // Test to see if it's right.
   return new Promise(res => res(fileName));
@@ -26,11 +52,19 @@ async function takeScreenshotToCompare(page, route, filePrefix) {
 
 async function takeOriginalScreenshot(page, route, filePrefix) {
   // If you didn't specify a file, use the name of the route.
-  let fileName = filePrefix + "/" + (route ? route : "index");
+  let fileName =
+    filePrefix +
+    "/" +
+    (index
+      ? index.charAt(0 === "/") && index.replace(/\//g, "-").substr(1)
+      : "index");
 
   // Start the browser, go to that page, and take a screenshot.
-  await page.goto(TEST_DIFFERENT_ROUTE);
-  await page.screenshot({ path: `${ORIGINAL_DIR}/${fileName}.png` });
+  await page.goto(route);
+  await page.screenshot({
+    path: `${ORIGINAL_DIR}/${fileName}.png`,
+    fullPage: true,
+  });
 
   return new Promise(res => res());
 }
@@ -76,7 +110,7 @@ function compareScreenshots(fileName) {
 }
 
 describe("take original screenshots", function() {
-  let browser, page;
+  let browser, page, URLS;
 
   // This is ran when the suite starts up.
   before(async function() {
@@ -85,13 +119,15 @@ describe("take original screenshots", function() {
 
     // Create the test directory if needed. This and the ORIGINAL_DIR
     // variables are global somewhere.
-    if (!fs.existsSync(ORIGINAL_DIR)) await fs.mkdirSync(ORIGINAL_DIR);
+    if (!fs.existsSync(ORIGINAL_DIR)) fs.mkdirSync(ORIGINAL_DIR);
 
     // And its wide screen/small screen subdirectories.
     if (!fs.existsSync(`${ORIGINAL_DIR}/wide`))
-      await fs.mkdirSync(`${ORIGINAL_DIR}/wide`);
+      fs.mkdirSync(`${ORIGINAL_DIR}/wide`);
     if (!fs.existsSync(`${ORIGINAL_DIR}/narrow`))
-      await fs.mkdirSync(`${ORIGINAL_DIR}/narrow`);
+      fs.mkdirSync(`${ORIGINAL_DIR}/narrow`);
+
+    URLS = await readFileLineByLine(TXT_FILE);
   });
 
   beforeEach(async function() {
@@ -102,24 +138,32 @@ describe("take original screenshots", function() {
   afterEach(() => browser.close());
 
   // Wide screen tests!
-  describe("wide screen", function() {
+  describe("wide screen", async function() {
     beforeEach(async function() {
       await page.setViewport({ width: 800, height: 600 });
     });
-    it("/view1", async function() {
-      await takeOriginalScreenshot(page, "view1", "wide");
-    });
+    await Promise.all(
+      URLS.map((pageObj, i) =>
+        it(url, async function() {
+          await takeOriginalScreenshot(page, `${url}`, i, "wide");
+        }).timeout(0),
+      ),
+    );
     // And your other routes, 404, etc.
   });
 
   // Narrow screen tests are the same, but with a different viewport.
-  describe("narrow screen", function() {
+  describe("narrow screen", async function() {
     beforeEach(async function() {
       await page.setViewport({ width: 375, height: 667 });
     });
-    it("/view1", async function() {
-      await takeOriginalScreenshot(page, "view1", "narrow");
-    });
+    await Promise.all(
+      URLS.map((pageObj, i) =>
+        it(url, async function() {
+          await takeOriginalScreenshot(page, `${url}`, i, "narrow");
+        }).timeout(0),
+      ),
+    );
     // And your other routes, 404, etc.
   });
 });
@@ -134,13 +178,12 @@ describe("compare new screenshots", function() {
 
     // Create the test directory if needed. This and the ORIGINAL_DIR
     // variables are global somewhere.
-    if (!fs.existsSync(TEST_DIR)) await fs.mkdirSync(TEST_DIR);
+    if (!fs.existsSync(TEST_DIR)) fs.mkdirSync(TEST_DIR);
 
     // And its wide screen/small screen subdirectories.
-    if (!fs.existsSync(`${TEST_DIR}/wide`))
-      await fs.mkdirSync(`${TEST_DIR}/wide`);
+    if (!fs.existsSync(`${TEST_DIR}/wide`)) fs.mkdirSync(`${TEST_DIR}/wide`);
     if (!fs.existsSync(`${TEST_DIR}/narrow`))
-      await fs.mkdirSync(`${TEST_DIR}/narrow`);
+      fs.mkdirSync(`${TEST_DIR}/narrow`);
   });
 
   beforeEach(async function() {
