929158d6573dea0576b1487e47826863c7c48043
Adam Winick <awinick@criticalmass.com>
2019-04-29 16:47:43 -0400

    finished POC with async tests in mocha

--- test/test.js
+++ test/test.js
@@ -15,6 +15,10 @@ const {
   TXT_FILE,
 } = CONSTANTS;
 
+const logBrowserIsClosed = () => {
+  console.log("Headless Chrome browser is closed");
+};
+
 // mocha -g 'take'
 // mocha -g 'compare'
 
@@ -28,17 +32,18 @@ const readFileLineByLine = async txtFile => {
   return _URLS;
 };
 
-async function takeOriginalScreenshot(page, route, filePrefix) {
+async function takeScreenshot(page, route, filePrefix, dir, original) {
   // If you didn't specify a file, use the name of the route.
-  console.log("ROUTE: ", route, "FILE PREFIX: ", filePrefix);
+  // console.log("ROUTE: ", route, "FILE PREFIX: ", filePrefix);
   let fileName = route.replace(/\//g, "-").substr(0);
 
   // go to a page, and take a screenshot.
   await page.goto(route, { timeout: 0, waitUntil: "domcontentloaded" });
   await page.screenshot({
-    path: `${ORIGINAL_DIR}/${filePrefix}/${fileName}.png`,
+    path: `${dir}/${filePrefix}/${fileName}.png`,
     fullPage: true,
   });
+  if (!original) return fileName;
   return;
 }
 
@@ -52,29 +57,88 @@ const makeDirectories = async directoryName => {
     fs.mkdirSync(`${directoryName}/narrow`);
 };
 
-describe("take original screenshots", function() {
-  let browser, page, URLS;
+const compareScreenshots = async fileName => {
+  return new Promise((resolve, reject) => {
+    const img1 = fs
+      .createReadStream(`${TEST_DIR}/wide/${fileName}.png`)
+      .pipe(new PNG())
+      .on("parsed", doneReading);
+    const img2 = fs
+      .createReadStream(`${ORIGINAL_DIR}/wide/${fileName}.png`)
+      .pipe(new PNG())
+      .on("parsed", doneReading);
 
-  const getUrls = async () => {
-    URLS = await readFileLineByLine(TXT_FILE);
-  };
+    let filesRead = 0;
+    function doneReading() {
+      // Wait until both files are read.
+      if (++filesRead < 2) return;
 
-  // Create the test directory if needed. This and the ORIGINAL_DIR
-  before(async () => {
-    makeDirectories(ORIGINAL_DIR);
+      // The files should be the same size.
+      expect(img1.width, "image widths are the same").equal(img2.width);
+      expect(img1.height, "image heights are the same").equal(img2.height);
+
+      // Do the visual diff.
+      const diff = new PNG({ width: img1.width, height: img2.height });
+      diff.pack().pipe(fs.createWriteStream(`${TEST_DIR}/${fileName}diff.png`));
+
+      const numDiffPixels = pixelmatch(
+        img1.data,
+        img2.data,
+        diff.data,
+        img1.width,
+        img1.height,
+        { threshold: 0.1 },
+      );
+
+      // The files should look the same.
+      expect(numDiffPixels, "number of different pixels").equal(0);
+    }
+    resolve();
   });
+};
 
-  it("screenshots done", async function(done) {
-    await getUrls();
-    console.log(URLS);
+const getUrls = async () => await readFileLineByLine(TXT_FILE);
+
+describe("take original screenshots", function() {
+  it("screenshots done", async function() {
+    await makeDirectories(ORIGINAL_DIR);
+    const URLS = await getUrls();
+    // launch headless chrome
+    const browser = await puppeteer.launch();
+
     Promise.all(
       URLS.map(async (url, i) => {
-        browser = await puppeteer.launch();
-        page = await browser.newPage();
+        const page = await browser.newPage();
+
+        // set viewPort size and take screenshot
         await page.setViewport({ width: 1920, height: 1024 });
-        await takeOriginalScreenshot(page, url, "wide");
-        await browser.close();
+        await takeScreenshot(page, url, "wide", ORIGINAL_DIR, true);
       }),
-    ).then(done);
+    ).then(async () => {
+      await browser.close().then(logBrowserIsClosed);
+    });
+  }).timeout(0);
+});
+
+describe("take and compare screenshots", function() {
+  it("screenshots done", async function() {
+    await makeDirectories(TEST_DIR);
+    const URLS = await getUrls();
+
+    // launch headless chrome
+    const browser = await puppeteer.launch();
+
+    return Promise.all(
+      URLS.map(async (url, i) => {
+        const page = await browser.newPage();
+
+        // set viewPort size and take screenshot
+        await page.setViewport({ width: 1920, height: 1024 });
+        const fileName = await takeScreenshot(page, url, "wide", TEST_DIR);
+        await compareScreenshots(fileName);
+      }),
+    ).then(async () => {
+      await browser.close().then(logBrowserIsClosed);
+    });
   }).timeout(0);
 });
