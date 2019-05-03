ded68456866d514ed9339b5c8e025dc535cb2249
Adam Winick <awinick@criticalmass.com>
2019-05-03 09:51:07 -0400

    added async fetch and xml parsing

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-const TEST_DIR = "screenshots-current";
-const ORIGINAL_DIR = "screenshots-originals";
-const TXT_FILE = "nl-urls.mini.txt";
-
-module.exports = {
-  TEST_DIR,
-  ORIGINAL_DIR,
-  TXT_FILE,
-};
